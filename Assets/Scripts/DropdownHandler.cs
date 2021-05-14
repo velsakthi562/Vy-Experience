9ddfe3cd1295c3faf492623708bf5f410a4fc786
vyvoxel <62131636+velsakthi562@users.noreply.github.com>
2021-05-14 20:32:30 +0530

    Updates

--- Assets/Scripts/DropdownHandler.cs
+++ Assets/Scripts/DropdownHandler.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DropdownHandler : MonoBehaviour
 {
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform mainMenu, contentMenu, viewMenu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.DOAnchorPos(Vector2.zero, 0.25f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeMenu()
+    {
+        mainMenu.DOAnchorPos(new Vector2(-1145, 0), 0.25f);
+        contentMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void BackMenu()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+        contentMenu.DOAnchorPos(new Vector2(1145, 0), 0.25f);
+    }
+}
